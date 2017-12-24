4f27d3e8dfa979a780697e2b14424e33e93121cf
Michael <michael.enjolras@yahoo.fr>
2017-12-24 02:21:14 +0100

    Parse html for tcl tram.

--- web/index.php
+++ web/index.php
@@ -52,14 +52,50 @@ $app->post('/hello', function () use ($app) {
 });
 
 
-// REST hello
+// REST tcl prochain tram
+
+//change to post and check how to use parameter part dieu / la soie
 $app->get('/tcl-prochain-tram', function () use ($app) {
     $tcl_prochain_tram_url='http://www.tcl.fr/Me-deplacer/Itineraires/Mon-trajet?ItinDepart=Reconnaissance+-+Balzac%2C+Lyon+3eme+%28Arr%C3%AAt%29&valueItinDepart=StopArea%7C2035%7CReconnaissance+-+Balzac%7CLyon+3eme%7C%7C%7C798248%7C2087000&valueItinDepartFavoris=StopArea%7Ctcl5560%7CReconnaissance+-+Balzac%7CLyon+3eme%7C%7C%7C798248%7C2087000&ItinArrivee=Relais+Info+Service+TCL+Part+Dieu+Villette%2C+Lyon+3eme&valueItinArrivee=Site%7C1537%7CRelais+Info+Service+TCL+Part+Dieu+Villette%7CLyon+3eme%7C%7C%7C796432%7C2087617%7C4309%2150%211%3B&valueItinArriveeFavoris=Site%7Ctcl21850%7CRelais+Info+Service+TCL+Part+Dieu+Villette%7CLyon+3eme%7C%7C%7C796432%7C2087617%7Ctcl35659%2150%211%3B&radioTiming=DepartImm&DepartMinute=00&radioSens=HorPartir&radioOption=OptionArrivRapid&lancer_recherche=Rechercher';
 
     $homepage = file_get_contents($tcl_prochain_tram_url);
-    $dom = new DOMDocument;
-    $dom->loadHTML($homepage);
-    $trajet=$dom->getElementById('RESULT-trajet');
+
+    $pos = strpos($homepage, 'class="depart"');
+    $depart = substr($homepage,$pos);
+    $pos = strpos($depart, "</span>");
+    $depart = substr($depart,15,$pos-15);
+
+    $pos = strpos($homepage, 'class="arrivee"');
+    $arrivee = substr($homepage,$pos);
+    $pos = strpos($arrivee, "</span>");
+    $arrivee = substr($arrivee,0+16,$pos-16);
+
+
+    $pos = strpos($homepage, 'TABLE-result-trajet');
+    $images= substr($homepage,$pos);
+    $pos = strpos($images, "</table>");
+    $images = substr($images,0,$pos);
+
+    $pos = strpos($images, '.png');
+    $png= substr($images,$pos+4);
+    $pos = strpos($png, ".png");
+    $png= substr($png,0,$pos);
+
+    $pos = strpos($png, 'src="');
+    $png= substr($png,$pos+5);
+
+    $splite = explode('/',$png);
+
+    $type=$splite[sizeof($splite)-1];
+    $depart = str_replace("h","heure",$depart);
+    $arrivee = str_replace("h","heure",$arrivee);
+
+    echo $depart;
+    echo $arrivee;
+    echo $type;
+    exit;
+
+    // change the retrun for google home
 
     $result = array("hello" => 'hello micka');
     return $app->json($result, 200);
