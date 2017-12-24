8f41b683edee3bdb18925644a5e4adb9080be676
Michael <michael.enjolras@yahoo.fr>
2017-12-24 16:20:39 +0100

    content type for resquest.

--- web/index.php
+++ web/index.php
@@ -55,6 +55,13 @@ $app->before(function() use ($app)
     }
 });
 
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
+
 /////////////////////
 //TODO
 
