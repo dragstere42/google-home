8c4cd224ec263578b48052881a91b42bc54d8aee
Michael <michael.enjolras@yahoo.fr>
2018-03-17 11:41:38 +0100

    Add change chaine function.

--- web/index.php
+++ web/index.php
@@ -485,5 +485,17 @@ $app->get('/train', function () use ($app) {
 
 });
 
+// change chaine
+$app->get('/change_chaine/{chaine}', function ($chaine) use ($app) {
+    
+    for($i=0; $i<strlen($chaine); $i++) {
+        $number = substr($chaine,$i,1);
+        $command = escapeshellcmd('/home/pi/domoticz/scripts/broadlink/play.py box_'.$number.'.txt');
+        shell_exec($command);
+    }
+    return $app->json($chaine, 200);
+
+});
+
 $app['debug'] = true;
 $app->run();
